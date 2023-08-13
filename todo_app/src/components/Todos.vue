7421ea6585ac3d48e9b746e8322352f6e5ba18c1
Valetina-2608 <valusha2608@gmail.com>
2023-08-13 22:30:33 +0300

    Add Todos

---
+++ todo_app/src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="block_app">
+      <div class="caption">Vue Todo App</div>
+  </div>
+   
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+<style scoped src="../assets/css/style.css">
+/* @import '../assets/css/style.css'; */
+</style>
