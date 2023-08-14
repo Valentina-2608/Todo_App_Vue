8c691d217f3184ec376c13704bbee479da5dd452
Valetina-2608 <valusha2608@gmail.com>
2023-08-14 18:35:57 +0300

    Add ToDos

---
+++ todo_app/src/components/ToDos.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="block_app">
+      <div class="caption">Vue Todo App</div>
+      <ul>
+          <li v-bind:key="item.id" v-for="item in todoEntries">
+              <ToDoItem v-bind:todoItem="item" />
+          </li>   
+      </ul>
+      
+  </div>
+   
+</template>
+
+<script>
+import ToDoItem from "./ToDoItem.vue";
+export default {
+  name: 'ToDos',
+  components: {
+      ToDoItem
+  },
+  props: [
+    "todoEntries"
+]
+}
+</script>
+
+
+<style scoped src="../assets/css/style.css">
+/* @import '../assets/css/style.css'; */
+</style>
