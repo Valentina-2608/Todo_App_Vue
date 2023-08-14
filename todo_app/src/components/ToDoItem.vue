35cef83dcc291056b5437cec8228969f68dd375a
Valetina-2608 <valusha2608@gmail.com>
2023-08-14 18:28:45 +0300

    Add ToDoItem

---
+++ todo_app/src/components/ToDoItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      {{ todoItem.title }}
+    </div>
+</template>
+  
+<script>
+  
+export default {
+  name: 'ToDoItem',
+  components: {
+  },
+  props: [
+    "todoItem"
+  ]
+}
+</script>
